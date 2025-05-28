89a5cd2bc0a3e5da6605f49e4496786e3fa19bdd
QwertyJacob <jesusfcevallos@gmail.com>
2025-05-28 15:22:39 +0000

    active inference WIP

--- neural_modules.py
+++ neural_modules.py
@@ -20,6 +20,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class PolicyNet(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(PolicyNet, self).__init__()
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, action_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x), dim=1)
+        return x
+
+
+class ValueNet(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(ValueNet, self).__init__()
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, action_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
+    
+
 class DQN(nn.Module):
     def __init__(self, state_size, action_size):
         super(DQN, self).__init__()
--- tiger_agents.py
+++ tiger_agents.py
@@ -1,9 +1,100 @@
-from smartController.neural_modules import DQN
+from smartController.neural_modules import DQN, PolicyNet, ValueNet
 import torch.optim as optim
 from collections import deque
 import torch
 import random
 import torch.nn as nn
+import torch.distributions as distributions
+import torch.functional as F
+
+
+class DAIAgent:
+    def __init__(self, kwargs):
+        
+        self.valuenet = ValueNet(kwargs['state_size'], kwargs['action_size'])
+        self.target_valuenet = ValueNet(kwargs['state_size'], kwargs['action_size'])
+        self.update_target_valuenet()
+        self.valuenet_optimizer = optim.Adam(self.valuenet.parameters(), lr=kwargs['lr'])
+                
+        self.policynet = PolicyNet(kwargs['state_size'], kwargs['action_size'])
+        self.policynet_optimizer = optim.Adam(self.policynet.parameters(), lr=kwargs['lr'])
+
+        self.memory = deque(maxlen=kwargs['memory_size'])
+        self.replay_batch_size = kwargs['replay_batch_size']
+
+        self.value_loss_fn = nn.MSELoss()
+
+
+    def update_target_valuenet(self):
+        self.target_valuenet.load_state_dict(self.valuenet.state_dict())
+
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+
+    def act(self, state):
+
+        action_probs = self.policynet(state)
+        
+        # sample from a categorical distribution 
+        m = distributions.Categorical(action_probs)
+        action = m.sample().item()
+
+        return action
+    
+
+    def train_actor(self, states):
+        """
+        Trains the actor (policy network)
+        """
+        policy_probabilities = self.policynet(states)
+
+        estimated_state_values = self.valuenet(states).detach()
+        values_logprobs = F.log_softmax(estimated_state_values, dim=1)
+        expected_logprobs = torch.sum(policy_probabilities * values_logprobs, dim=1)
+        critic_losses = -expected_logprobs.mean(dim=1)
+
+        self.policynet_optimizer.zero_grad()
+        critic_loss = critic_losses.mean()
+        critic_loss.backward()
+        self.policynet_optimizer.step()
+
+
+    def replay(self):
+        """
+        Trains the critic (value network)
+        """
+        if len(self.memory) < self.replay_batch_size:
+            return
+        
+        minibatch = random.sample(self.memory, self.replay_batch_size)
+
+        for state, action, reward, next_state, done in minibatch:
+
+            target = reward
+            
+            if not done:
+                
+                # Select action using online network
+                policy_probabilities = self.policynet(next_state)
+                estimated_state_values = self.valuenet(next_state)
+
+                # Compute expected value under the policy
+                expected_value = torch.sum(policy_probabilities * estimated_state_values, dim=1)
+
+                # Update target
+                target += 0.99 * expected_value.detach()
+
+            target_f = self.valuenet(state).detach()
+            target_f[action] = target
+
+            self.valuenet_optimizer.zero_grad()
+            predicted_values = self.valuenet(state)
+            value_losses = self.value_loss_fn(predicted_values, target_f)
+            value_losses.backward()
+            self.valuenet_optimizer.step()
+    
 
 class ValueLearningAgent:
 
