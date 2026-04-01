86dd8d24b2ebfe71af65ce9b15f67173a988104f
QwertyJacob <jesusfcevallos@gmail.com>
2026-04-01 20:20:16 +0000

    refac

--- tiger_brain_new.py
+++ tiger_brain_new.py
@@ -118,7 +118,9 @@ class TigerBrain:
         self.use_neural_KR = args.intrusion_detection.use_neural_KR
         self.use_neural_CS = args.intrusion_detection.use_neural_CS
         self.online_evaluation = args.intrusion_detection.online_evaluation
+        self.wrong_inference_penalisation = args.intrusion_detection.bad_classif_penalisation
         self.bad_classif_cost_factor = float(args.intrusion_detection.bad_classif_cost_factor)
+        self.bad_clustering_cost_factor = float(args.intrusion_detection.hard_bad_classif_cost_factor)
         self.online_eval_rounds = int(args.intrusion_detection.online_evaluation_rounds)
         self.load_pretrained_inference_module = args.intrusion_detection.pretrained_inference
         self.clustering_loss_backprop = args.intrusion_detection.clustering_loss_backprop
@@ -644,7 +646,7 @@ class TigerBrain:
 
         if action_signal.item() == 0:
             correct_classif_rewards = torch.abs(known_samples_costs * correct_mask)
-            if self.intrusion_detection_kwargs['bad_classif_penalisation'] == 'easy':
+            if self.wrong_inference_penalisation == 'easy':
                 bad_classif_costs = -torch.abs(known_samples_costs * (~correct_mask))
             else:
                 bad_classif_costs = -torch.abs(known_samples_costs * (~correct_mask) * self.bad_classif_cost_factor)
@@ -731,12 +733,12 @@ class TigerBrain:
 
             if accepted_cluster: 
                 cost = cost_if_acc[~missing][idx]
-                f = float(self.intrusion_detection_kwargs['hard_bad_classif_cost_factor']) if self.intrusion_detection_kwargs['bad_classif_penalisation'] == 'hard' else 1.0
+                f = self.bad_clustering_cost_factor if self.wrong_inference_penalisation == 'hard' else 1.0
                 current_reward += f * cost
                 current_reward += benign_per_cluster[~missing][idx].item()
             else:
                 cost = self.bad_classif_cost_factor * benign_per_cluster[~missing][idx]
-                f = float(self.intrusion_detection_kwargs['hard_bad_classif_cost_factor']) if self.intrusion_detection_kwargs['bad_classif_penalisation'] == 'hard' else 1.0
+                f = self.bad_clustering_cost_factor if self.wrong_inference_penalisation == 'hard' else 1.0
                 current_reward -= f * cost
             
             if epistemic_action:
